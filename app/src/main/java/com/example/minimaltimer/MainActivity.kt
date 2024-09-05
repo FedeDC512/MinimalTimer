3095b878bbef4224dd5aad81778b4f1889ac6038
Federico Agnello <78120567+FedeDC512@users.noreply.github.com>
2024-09-05 23:43:29 +0200

    Working countdown from user input and basic ui

--- app/src/main/java/com/example/minimaltimer/MainActivity.kt
+++ app/src/main/java/com/example/minimaltimer/MainActivity.kt
@@ -1,50 +1,41 @@
 package com.example.minimaltimer
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.minimaltimer.ui.theme.MinimalTimerTheme
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Row
-import androidx.compose.ui.res.painterResource
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Surface
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.border
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
-import android.content.res.Configuration
-//import android.icu.util.TimeUnit
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.clickable
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.animation.animateColorAsState
-import androidx.compose.animation.animateContentSize
-import androidx.compose.foundation.horizontalScroll
-import androidx.compose.material3.Button
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.minimaltimer.ui.theme.MinimalTimerTheme
 import kotlinx.coroutines.delay
 import java.util.concurrent.TimeUnit
 
@@ -63,8 +54,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Timer(){
-    var time by remember {
+    var timeInput by remember {
         mutableStateOf(0L)
+    }
+
+    var minutesInput by remember {
+        mutableStateOf("")
+    }
+
+    var secondsInput by remember {
+        mutableStateOf("")
+    }
+
+    var count by remember {
+        mutableStateOf(0)
+    }
+
+    var time by remember {
+        mutableStateOf(timeInput)
     }
 
     var isRunning by remember {
@@ -74,44 +81,66 @@ fun Timer(){
     var startTime by remember {
         mutableStateOf(0L)
     }
+
+    var isDialogOpen by remember { mutableStateOf(false) }
+
+    fun showDialog() {
+        isDialogOpen = true
+    }
+
+    fun hideDialog() {
+        isDialogOpen = false
+    }
     
     val context = LocalContext.current
 
     val keyboardController = LocalSoftwareKeyboardController.current
 
-    Column (
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(15.dp)
-    ) {
-        Text(
-            text = formatTime(timeInMillis = time),
-            style = MaterialTheme.typography.headlineLarge,
-            modifier = Modifier.padding(9.dp)
-        )
-
-        Spacer(modifier = Modifier.height(18.dp))
-
-        Row {
-            Button(onClick = {
-                if (isRunning) {
-                    isRunning = false
-                } else {
-                    startTime = System.currentTimeMillis() - time
-                    isRunning = true
-                    keyboardController?.hide()
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .clickable {
+            count++
+        },
+    ){
+        Text(text = count.toString())
+        Column (
+            modifier = Modifier
+                .align(Alignment.Center)
+                .padding(15.dp)
+        ) {
+            Row {
+                Spacer(modifier = Modifier.weight(1f))
+                Box (modifier = Modifier
+                    .background(Color.Yellow)
+                    .clickable { showDialog() },
+                ) {
+                    Text(
+                        text = formatTime(timeInMillis = time),
+                        style = MaterialTheme.typography.headlineLarge
+                    )
                 }
-                }) {
-                    Text(text = if (isRunning) "Pause" else "Start", color = Color.White)
+                Spacer(modifier = Modifier.weight(1f))
             }
 
-            Spacer(modifier = Modifier.width(16.dp))
+            Spacer(modifier = Modifier.height(18.dp))
 
-            Button(onClick = {
-                time = 0
-                isRunning = false
-            }) {
-                Text(text = "Reset", color = Color.White)
+            Row {
+                Spacer(modifier = Modifier.weight(1f))
+
+                Button(onClick = {
+                    if (isRunning) {
+                        time = 0L
+                        isRunning = false
+                    } else if (time != 0L) {
+                        startTime = System.currentTimeMillis() - time
+                        isRunning = true
+                        keyboardController?.hide()
+                    }
+                    }) {
+                        Text(text = if (isRunning) "Reset" else "Start", color = MaterialTheme.colorScheme.surface)
+                }
+
+                Spacer(modifier = Modifier.weight(1f))
             }
         }
     }
@@ -119,18 +148,89 @@ fun Timer(){
     LaunchedEffect(isRunning) {
         while (isRunning){
             delay(1000)
-            time = System.currentTimeMillis()-startTime
+            time -= 1000
         }
 
+    }
+
+    // Finestra di dialogo
+    if (isDialogOpen) {
+        AlertDialog(
+            onDismissRequest = { hideDialog() },
+            title = { Text("Enter Time") },
+            text = {
+                Row {
+                    OutlinedTextField(
+                        value = minutesInput,
+                        onValueChange = { newValue ->
+                            minutesInput = newValue
+                        },
+                        label = { Text("mm") },
+                        keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
+                        modifier = Modifier
+                            .width(80.dp)
+                            .padding(8.dp)
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+
+                    OutlinedTextField(
+                        value = secondsInput,
+                        onValueChange = { newValue ->
+                            secondsInput = newValue
+                        },
+                        label = { Text("ss") },
+                        keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
+                        modifier = Modifier
+                            .width(80.dp)
+                            .padding(8.dp)
+                    )
+                }
+            },
+            confirmButton = {
+                Button(
+                    onClick = {
+                        hideDialog()
+                        isRunning = false
+                        time = parseTimeToMillis(minutesInput, secondsInput)
+                    }
+                ) {
+                    Text("Confirm")
+                }
+            },
+            dismissButton = {
+                Button(onClick = { hideDialog() }) {
+                    Text("Cancel")
+                }
+            }
+        )
     }
 
 }
 
 @Composable
 fun formatTime(timeInMillis: Long): String {
-    val hours = TimeUnit.MILLISECONDS.toHours(timeInMillis)
-    val minutes = TimeUnit.MILLISECONDS.toMinutes(timeInMillis) % 60
+    val minutes = TimeUnit.MILLISECONDS.toMinutes(timeInMillis)
     val seconds = TimeUnit.MILLISECONDS.toSeconds(timeInMillis) % 60
 
-    return String.format("%02d:%02d:%02d", hours, minutes, seconds)
+    return String.format("%02d:%02d", minutes, seconds)
+}
+
+fun parseTimeToMillis(minutes: String, seconds: String): Long {
+    val minutesValue = minutes.toIntOrNull() ?: 0
+    val secondsValue = seconds.toIntOrNull() ?: 0
+    return TimeUnit.MINUTES.toMillis(minutesValue.toLong()) + TimeUnit.SECONDS.toMillis(secondsValue.toLong())
+}
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
+@Composable
+fun GreetingPreview() {
+    MinimalTimerTheme {
+        Surface(modifier = Modifier.fillMaxSize()) {
+            Timer()
+        }
+    }
 }
